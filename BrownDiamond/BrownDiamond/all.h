7a7a92ff698394c84b02935cba01fa8ffb93eb11
stackprobe <stackprobes@gmail.com>
2019-07-25 23:02:16 +0900

    Rosetta

---
+++ BrownDiamond/BrownDiamond/all.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Common\all.h"
+
+#include "ProcMain.h"
+#include "Test0001.h"
---
+++ _Clean.bat
@@ -0,0 +1,2 @@
+C:\Factory\Tools\RDMD.exe /RM out
+DEL BrownDiamond\BrownDiamond\SaveData.dat
