0a5a2c9ae9fcfbbeda1bbf7ab06df4d833ec2628
stackprobe <stackprobes@gmail.com>
2019-08-29 08:10:08 +0900

    March

--- BrownDiamond/BrownDiamond/all.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Common\all.h"
-
-#include "ProcMain.h"
-#include "Test0001.h"
--- _Release.bat
+++ _Release.bat
@@ -1,25 +1,16 @@
 C:\Factory\Tools\RDMD.exe /RC out
 
-SET RAWKEY=ffffffffffffffffffffffffffffffff
-rem $_git:secretBegin
-///////////////////////////////////////////
-rem $_git:secretEnd
+C:\Factory\SubTools\makeDDResourceFile.exe Resource out\Resource.dat Tools\MaskGZData.exe
 
-C:\Factory\SubTools\makeAESCluster.exe Picture.txt     out\Picture.dat     %RAWKEY% 110000000
-C:\Factory\SubTools\makeAESCluster.exe Music.txt       out\Music.dat       %RAWKEY% 120000000
-C:\Factory\SubTools\makeAESCluster.exe SoundEffect.txt out\SoundEffect.dat %RAWKEY% 130000000
-C:\Factory\SubTools\makeAESCluster.exe Etcetera.txt    out\Etcetera.dat    %RAWKEY% 140000000
-C:\Factory\SubTools\makeAESClusterForSH.exe Storehouse out\Storehouse.dat  %RAWKEY% 150000000
-
-COPY /B BrownDiamond\Release\BrownDiamond.exe out\BrownDiamond.exe
-
-out\BrownDiamond.exe /L
-IF ERRORLEVEL 1 START ?_LOG_ENABLED
+C:\Factory\SubTools\CallConfuserCLI.exe BrownDiamond\BrownDiamond\bin\Release\BrownDiamond.exe out\BrownDiamond.exe
+rem COPY /B BrownDiamond\BrownDiamond\bin\Release\BrownDiamond.exe out
+COPY /B BrownDiamond\BrownDiamond\bin\Release\Chocolate.dll out
+COPY /B BrownDiamond\BrownDiamond\bin\Release\DxLib.dll out
+COPY /B BrownDiamond\BrownDiamond\bin\Release\DxLib_x64.dll out
+COPY /B BrownDiamond\BrownDiamond\bin\Release\DxLibDotNet.dll out
 
 C:\Factory\Tools\xcp.exe doc out
-
-COPY /B AUTHORS out
-COPY /B C:\Dev\Game\Codevil\doc\Config.conf out
+C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut2\doc out
 
 C:\Factory\SubTools\zip.exe /G out BrownDiamond
 C:\Factory\Tools\summd5.exe /M out
