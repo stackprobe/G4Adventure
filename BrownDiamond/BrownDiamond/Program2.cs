3972066b3eaa0ab8f5356c5b20fe340cfb1b9e8f
stackprobe <stackprobes@gmail.com>
2019-10-06 12:07:42 +0900

    Ace

--- BrownDiamond/BrownDiamond/Program2.cs
+++ BrownDiamond/BrownDiamond/Program2.cs
@@ -72,9 +72,26 @@ namespace Charlotte
 
 		private void Main4()
 		{
+			if (ProcMain.ArgsReader.ArgIs("/D"))
+			{
+				Main4_Debug();
+			}
+			else
+			{
+				Main4_Release();
+			}
+		}
+
+		private void Main4_Debug()
+		{
 			//new Test0001().Test01();
 			//Title.TitleMain();
 			new GameTest().Test01();
+		}
+
+		private void Main4_Release()
+		{
+			Title.TitleMain();
 		}
 	}
 }
